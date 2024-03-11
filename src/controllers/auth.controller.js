feeb5dd93ed9ff02ec9f8a6cc3ddfa2f27931894
Jaimevs <jaime.vqzan@gmail.com>
2024-03-10 23:33:35 -0600

    token creation with jsonwebtoken

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,21 +2,30 @@ import bcrypt from 'bcrypt'
 
 import {pool} from '../db.js'
 
+import {createAccessToken} from '../libs/jwt.js'
+
 export const signin=(req,res)=>res.send('ingresando')
 
 //----------------------------------------------------
 //registrando
 export const sigup=async(req,res)=>{
+
     const {name, email, password} = req.body
     
     try{
         const hashedPassword=await bcrypt.hash(password, 10)
-        console.log(hashedPassword);
+        //console.log(hashedPassword);
 
         const result=await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) Returning *',[name, email, hashedPassword])
 
-        console.log(result);
-        return res.json(result.rows[0])
+        const token = await createAccessToken({id: result.rows[0].id})
+
+        //console.log(result);
+        
+        //return res.json(result.rows[0])
+        return res.json({
+            token: token,
+        })
     }catch(error){
         if(error.code=='23505'){
             return res.status(400).json({
